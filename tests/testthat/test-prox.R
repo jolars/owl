01bb5fac6b0c4c0e78a81ab0b2c57d0252811a40
Johan Larsson <mail@larssonjohan.com>
2019-02-10 19:42:25 +0100

    Add tests, change license to GPL-3, add suggest for tests

---
+++ tests/testthat/test-prox.R
@@ -0,0 +1,33 @@
+context("prox")
+
+test_that("prox_sorted_L1 agrees with isotone package", {
+  if (!requireNamespace("isotone", quietly = TRUE))
+    skip("isotone is not available")
+  if (!requireNamespace("SLOPE", quietly = TRUE))
+    skip("SLOPE is not available")
+
+  prox_slope_isotone <- function(y, lambda) {
+    n <- length(y)
+
+    # Solve the quadratic programming problem:
+    #   min ||y - lambda - x||_2 s.t. x_1 >= x_2 >= ... >= x_n
+    result <- isotone::activeSet(cbind(2:n, 1:(n-1)),
+                                 y = y - lambda,
+                                 weights = rep(1, n))
+
+    # Enforce non-negativity constraint.
+    pmax(result$x, 0)
+  }
+
+
+  n <- 20
+  mu <- 1.5*(n:1)
+  y <- sort(abs(rnorm(n, mean = mu)), decreasing = TRUE)
+  lambda <- n:1
+
+  expect_equivalent(golem:::prox_slope_cpp(y, lambda),
+                    prox_slope_isotone(y, lambda))
+  expect_equivalent(golem:::prox_slope_cpp(y, lambda),
+                    SLOPE:::prox_sorted_L1(y, lambda))
+})
+
---
+++ tests/testthat/test-slope.R
@@ -0,0 +1,21 @@
+context("test-slope")
+
+test_that("multiplication works", {
+  if (!requireNamespace("SLOPE", quietly = TRUE))
+    skip("SLOPE package not available")
+
+  set.seed(1)
+
+  problem <- golem:::random_problem(10000, 50, sigma=1)
+
+  X <- problem$X
+  y <- problem$y
+
+  set.seed(0)
+  res_slope <- SLOPE::SLOPE(X, y, sigma = 1)
+  set.seed(0)
+  res_golem <- golem:::fit(X, y, sigma = 1)
+
+  expect_equivalent(res_slope$beta, res_golem$beta)
+})
+
