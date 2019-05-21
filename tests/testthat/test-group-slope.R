a81461de85bcebea738147dd088240d455415855
Johan Larsson <mail@larssonjohan.com>
2019-05-21 16:57:48 +0200

    Add test to check rudimentary behavior of group slope

--- tests/testthat/test-group-slope.R
+++ tests/testthat/test-group-slope.R
@@ -17,3 +17,23 @@ test_that("results from group slope mirror those from grpSLOPE package", {
   expect_equivalent(coef(golem_fit), coef(gslope_fit, scaled = FALSE),
                     tol = 1e-6)
 })
+
+test_that("binomial group slope models work", {
+  set.seed(1)
+  library(golem)
+
+  X <- scale(matrix(rnorm(3000), ncol = 3))
+  x1 <- X[, 1]
+  x2 <- X[, 2]
+  x3 <- X[, 3]
+  z <- 1 + 2*x1 + 3*x2 + x3
+  pr <- 1/(1 + exp(-z))
+  y <- rbinom(1000, 1, pr)
+
+  df <- data.frame(y = y, x1 = x1, x2 = x2, x3 = x3)
+  golem_fit <- golem(cbind(x1, x2, x3), y,
+                     family = "binomial",
+                     penalty = GroupSlope(groups = c(1, 1, 2), sigma = 3))
+
+  expect_equivalent(coef(golem_fit) != 0, c(TRUE, TRUE, TRUE, FALSE))
+})
