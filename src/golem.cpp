fd989813281e878ba1d0e3a9f854d3309897ba69
Johan Larsson <mail@larssonjohan.com>
2019-05-24 12:38:26 +0200

    Speed up weighting

--- src/golem.cpp
+++ src/golem.cpp
@@ -42,14 +42,8 @@ golemDense(arma::mat x,
 
   // apply weights
   auto weights = as<arma::vec>(control["weights"]);
-  // for (uword j = 0; j < p; ++j)
-  //   x.col(j) /= weights(j);
-  //
-  // weights.print();
-
-  mat Dinv = diagmat(1.0/weights);
-
-  x = x * Dinv;
+  for (uword j = 0; j < p; ++j)
+    x.col(j) /= weights(j);
 
   uvec passes(n_penalties);
   std::vector<std::vector<double>> primals;
@@ -65,7 +59,7 @@ golemDense(arma::mat x,
   for (uword i = 0; i < n_penalties; ++i) {
     Results res = solver.fit(x, y, family, penalty, fit_intercept);
 
-    betas.slice(i) = Dinv * res.beta;
+    betas.slice(i) = res.beta/weights;
     intercepts.slice(i) = res.intercept;
     passes(i) = res.passes;
     penalty->step(i + 1); // move a step on the regularization path
