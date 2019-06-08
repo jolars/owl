989e0f3203d3517faa653ee153b0fa7fca008f4f
Johan Larsson <mail@larssonjohan.com>
2019-06-08 21:15:28 +0200

    Enable plotting for diagnostics
    * Diagnostics is now a separate class
    * The Diagnostics class has a plot() function
    to make it easy to look at the optimization run
    * Some tests to make this work well

---
+++ tests/testthat/test-diagnostics.R
@@ -0,0 +1,10 @@
+test_that("diagnostics are working properly", {
+  xy <- golem:::randomProblem(100, 2, q = 1)
+
+  model <- golem(diagnostics = TRUE)
+  model$fit(xy$x, xy$y)
+
+  expect_is(model$diagnostics, "Diagnostics")
+  expect_is(model$diagnostics$data, "data.frame")
+  expect_silent(dont_plot(model$diagnostics$plot()))
+})
