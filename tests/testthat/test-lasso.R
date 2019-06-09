a9d1acf8da9e1132ea3c2ce1d0e2913574d2e6b5
Johan Larsson <mail@larssonjohan.com>
2019-06-09 21:02:54 +0200

    Add test that checks for equality between lasso and slope fits when all lambda are equal

--- tests/testthat/test-lasso.R
+++ tests/testthat/test-lasso.R
@@ -11,3 +11,20 @@ test_that("lasso induces sparse models", {
 
   expect_equivalent(glm_fit$lambda, fit$penalty$lambda, tol = 1e-4)
 })
+
+test_that("lasso and slope fits are equivalent if all lambda are equal", {
+  set.seed(1)
+  xy <- golem:::randomProblem(100, 10)
+  x <- xy$x
+  y <- xy$y
+
+  model <- golem(penalty = "lasso", lambda = 0.2)
+  model$fit(x, y)
+  lasso_coef <- model$coef()
+
+  model$fit(x, y, penalty = "slope",
+            lambda = rep(0.2, NCOL(x))*NROW(x), sigma = 1)
+  slope_coef <- model$coef()
+
+  expect_equal(lasso_coef, slope_coef)
+})
