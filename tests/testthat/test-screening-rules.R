5c9a5122dfa78652f39f77f6f409f28166644b1a
Johan Larsson <mail@larssonjohan.com>
2019-12-03 14:42:43 +0100

    Add test for screening rules

---
+++ tests/testthat/test-screening-rules.R
@@ -0,0 +1,13 @@
+test_that("screening rules return correct results for instances with known violations", {
+  set.seed(216) # there is a violation for this seed and this setup
+  d <- prague:::randomProblem(100, 10, q = 0.3)
+
+  fit <- prague::golem(d$x, d$y, screening_rule = "none")
+  coefs <- coef(fit)
+
+  for (rule in c("none", "safe", "strong")) {
+    fit <- prague::golem(d$x, d$y, screening_rule = rule, diagnostics = TRUE)
+    expect_equivalent(coefs, coef(fit))
+  }
+})
+
