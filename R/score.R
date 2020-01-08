11c9c8764efffb1f1900870d94763b38ce146971
Johan Larsson <mail@larssonjohan.com>
2020-01-08 10:09:46 +0100

    fix scoring

--- R/score.R
+++ R/score.R
@@ -1,3 +1,34 @@
+auc <- function(y, prob, weights = rep.int(1, nrow(y))) {
+  if (is.matrix(y) || is.data.frame(y)) {
+
+    ny <- nrow(y)
+    auc(rep(c(0, 1), c(ny, ny)), c(prob,prob), as.vector(weights*y))
+
+  } else {
+
+    if (is.null(weights)) {
+      rprob <- rank(prob)
+      n1 <- sum(y)
+      n0 <- length(y) - n1
+      u <- sum(rprob[y == 1]) - n1*(n1 + 1)/2
+      exp(log(u) - log(n1) - log(n0))
+    } else {
+      # randomize ties
+      rprob <- stats::runif(length(prob))
+      op <- order(prob, rprob)
+      y <- y[op]
+      weights <- weights[op]
+      cw <- cumsum(weights)
+      w1 <- weights[y == 1]
+      cw1 <- cumsum(w1)
+      wauc <- log(sum(w1 * (cw[y == 1] - cw1)))
+      sumw1 <- cw1[length(cw1)]
+      sumw2 <- cw[length(cw)] - sumw1
+      exp(wauc - log(sumw1) - log(sumw2))
+    }
+  }
+}
+
 #' Return the loss of a owl model
 #'
 #' This function is a unified interface to return various types of loss for a
@@ -29,9 +60,9 @@ score <- function(object, x, y, measure)
 #' @rdname score
 #' @export
 score.OwlGaussian <- function(object,
-                                x,
-                                y,
-                                measure = c("deviance", "mse", "mae")) {
+                              x,
+                              y,
+                              measure = c("deviance", "mse", "mae")) {
   measure <- match.arg(measure)
 
   y <- as.vector(y)
@@ -46,13 +77,13 @@ score.OwlGaussian <- function(object,
 #' @rdname score
 #' @export
 score.OwlBinomial <- function(object,
-                                x,
-                                y,
-                                measure = c("deviance",
-                                            "mse",
-                                            "mae",
-                                            "accuracy",
-                                            "auc")) {
+                              x,
+                              y,
+                              measure = c("deviance",
+                                          "mse",
+                                          "mae",
+                                          "misclass",
+                                          "auc")) {
   measure <- match.arg(measure)
 
   prob_min <- 1e-05
@@ -65,9 +96,7 @@ score.OwlBinomial <- function(object,
 
   switch(
     measure,
-    auc = {
-      apply(y_hat, 3, function(y_hat_i) auc(y, y_hat_i))
-    },
+    auc = apply(y_hat, 3, function(y_hat_i) auc(y, y_hat_i)),
     mse = apply((y_hat + y[, 1] - 1)^2 + (y_hat - y[, 2])^2, 3, mean),
     mae = apply(abs(y_hat + y[, 1] - 1) + abs(y_hat - y[, 2]), 3, mean),
     deviance = {
@@ -78,38 +107,46 @@ score.OwlBinomial <- function(object,
       ly <- drop((y * ly) %*% c(1, 1))
       apply(2 * (ly - lp), 3, mean)
     },
-    accuracy = 1 -
-      apply(y[, 1] * (y_hat > 0.5) + y[, 2] * (y_hat <= 0.5), 3, mean)
+    misclass = apply(y[, 1] * (y_hat > 0.5) + y[, 2] * (y_hat <= 0.5), 3, mean)
   )
 }
 
-auc <- function(y, prob, weights = rep.int(1, nrow(y))) {
-  if (is.matrix(y) || is.data.frame(y)) {
+#' @rdname score
+#' @export
+score.OwlMultinomial <- function(object,
+                                 x,
+                                 y,
+                                 measure = c("deviance",
+                                             "mse",
+                                             "mae",
+                                             "misclass")) {
+  measure <- match.arg(measure)
 
-    ny <- nrow(y)
-    auc(rep(c(0, 1), c(ny, ny)), c(prob,prob), as.vector(weights*y))
+  prob_min <- 1e-05
+  prob_max <- 1 - prob_min
 
-  } else {
+  y_hat <- stats::predict(object, x, type = "response", simplify = FALSE)
+  y_hat_class <- stats::predict(object, x, type = "class", simplify = FALSE)
 
-    if (is.null(weights)) {
-      rprob <- rank(prob)
-      n1 <- sum(y)
-      n0 <- length(y) - n1
-      u <- sum(rprob[y == 1]) - n1*(n1 + 1)/2
-      exp(log(u) - log(n1) - log(n0))
-    } else {
-      # randomize ties
-      rprob <- stats::runif(length(prob))
-      op <- order(prob, rprob)
-      y <- y[op]
-      weights <- weights[op]
-      cw <- cumsum(weights)
-      w1 <- weights[y == 1]
-      cw1 <- cumsum(w1)
-      wauc <- log(sum(w1 * (cw[y == 1] - cw1)))
-      sumw1 <- cw1[length(cw1)]
-      sumw2 <- cw[length(cw)] - sumw1
-      exp(wauc - log(sumw1) - log(sumw2))
-    }
-  }
+  y <- as.factor(y)
+  n_levels <- length(unique(y))
+
+  y <- diag(n_levels)[as.numeric(y), ]
+  y <- array(y, dim(y_hat))
+
+  switch(
+    measure,
+    mse = apply((y - y_hat)^2, c(1, 3), mean),
+    mae = apply(abs(y - y_hat), c(1, 3), mean),
+    deviance = {
+      y_hat <- pmin(pmax(y_hat, prob_min), prob_max)
+      pp <- pmin(pmax(y_hat, prob_min), prob_max)
+
+      lp <- y * log(y_hat)
+      ly <- y * log(y)
+      ly[y == 0] <- 0
+      apply(2 * (ly - lp), c(1, 3), sum)
+    },
+    misclass = apply(y[, 1] * (y_hat > 0.5) + y[, 2] * (y_hat <= 0.5), 3, mean)
+  )
 }
