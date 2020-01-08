9b92c9f4dc105df78b140ae3e447bd577ab33f60
Johan Larsson <mail@larssonjohan.com>
2020-01-08 10:07:39 +0100

    Fix print method

--- R/print.R
+++ R/print.R
@@ -1,12 +1,17 @@
 #' Print results from owl fit
 #'
 #' @param x an object of class `'Owl'` or `'TrainedOwl'`
-#' @param digits number of significant digits to print in results
 #' @param ... other arguments passed to [print()]
 #'
 #' @return Prints output on the screen
+#'
+#' @examples
+#' fit <- owl(wine$x, wine$y, family = "multinomial")
+#' print(fit, digits = 1)
+#'
+#' @method print Owl
 #' @export
-print.Owl <- function(x, digits = 2, ...) {
+print.Owl <- function(x, ...) {
   sigma <- x$sigma
   n_nonzero <- apply(x$nonzeros, 3, sum)
   deviance_ratio <- x$deviance_ratio
@@ -22,14 +27,15 @@ print.Owl <- function(x, digits = 2, ...) {
 
   # print path summary
   cat("\nPath summary:\n")
-  print(out, digits = digits, ...)
+  print(out, ...)
 
   # print lambda path
   cat("\nLambda sequence:\n")
-  print(as.vector(x$lambda), digits = digits, ...)
+  print(as.vector(x$lambda), ...)
 }
 
 #' @rdname print.Owl
+#' @method print TrainedOwl
 #' @export
 print.TrainedOwl <- function(x, ...) {
   cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
@@ -39,3 +45,4 @@ print.TrainedOwl <- function(x, ...) {
 
   print(x$optima, ...)
 }
+
