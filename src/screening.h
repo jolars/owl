a2b507da0a253e6336609ba1cf701d43dedecf12
Johan Larsson <mail@larssonjohan.com>
2020-01-12 21:04:50 +0100

    Correct error in screening rule

--- src/screening.h
+++ src/screening.h
@@ -4,8 +4,7 @@
 
 using namespace arma;
 
-uvec activeSet(const mat& y,
-               const mat& gradient_prev,
+uvec activeSet(const mat& gradient_prev,
                const vec& lambda,
                const vec& lambda_prev)
 {
@@ -16,15 +15,15 @@ uvec activeSet(const mat& y,
   vec abs_grad = abs(vectorise(gradient_prev));
   uvec ord = sort_index(abs_grad, "descend");
   vec abs_grad_sorted = abs_grad(ord);
+  vec tmp = abs_grad_sorted + lambda_prev - 2*lambda;
 
   uword i = 0;
   uword k = 0;
 
-  double s{0};
+  double s = 0;
 
   while (i + k < p) {
-
-    s += abs_grad_sorted(k) + lambda_prev(k) - 2*lambda(k);
+    s += tmp(k+i);
 
     if (s >= 0) {
       k = k + i + 1;
@@ -35,7 +34,7 @@ uvec activeSet(const mat& y,
     }
   }
 
-  active_set.head(k).fill(1);
+  active_set.head(k).ones();
   active_set(ord) = active_set;
 
   umat active_set_mat = reshape(active_set, size(gradient_prev));
