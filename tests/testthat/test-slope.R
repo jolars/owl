b68b44ebabbb43e862208be5ba7b75d7bcfc05e1
Johan Larsson <mail@larssonjohan.com>
2020-01-13 15:24:22 +0100

    Fix test for slope and owl comparison

--- tests/testthat/test-slope.R
+++ tests/testthat/test-slope.R
@@ -7,14 +7,16 @@ test_that("SLOPE and owl agree for gaussian designs", {
   y <- problem$y
 
   set.seed(0)
-  res_slope <- SLOPE::SLOPE(x, y, sigma = 1, normalize = FALSE)
+  res_slope <- SLOPE::SLOPE(x, y, sigma = 1, normalize = FALSE,
+                            fdr = 0.25)
   set.seed(0)
   g <- owl(x, y,
            intercept = FALSE,
            sigma = 1,
            lambda = "gaussian",
            diagnostics = TRUE,
-           standardize_features = FALSE)
+           standardize_features = FALSE,
+           q = 0.25)
 
   expect_equivalent(res_slope$beta, coef(g), tol = 1e-5)
 })
@@ -28,8 +30,10 @@ test_that("SLOPE and owl agree when computing lambda sequences", {
   y <- problem$y
 
   for (lambda in c("bhq", "gaussian")) {
-    slope_lambda <- SLOPE::SLOPE(x, y, sigma = 1, lambda = lambda)$lambda
-    owl_lambda <- owl(x, y, sigma = 1, lambda = lambda)$lambda*nrow(x)
+    slope_lambda <- SLOPE::SLOPE(x, y, sigma = 1, lambda = lambda,
+                                 fdr = 0.1)$lambda
+    owl_lambda <- owl(x, y, sigma = 1, lambda = lambda,
+                      q = 0.1)$lambda*nrow(x)
     expect_equivalent(owl_lambda, slope_lambda)
   }
 })
