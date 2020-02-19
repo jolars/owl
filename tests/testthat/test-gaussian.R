20264e95bbfb18fe3d0282b96f8a235df9baf1cd
Johan Larsson <mail@larssonjohan.com>
2020-02-19 09:30:47 +0100

    Add test to check combinations of wide and tall input

--- tests/testthat/test-gaussian.R
+++ tests/testthat/test-gaussian.R
@@ -13,5 +13,24 @@ test_that("unregularized gaussian models work as expected", {
 
   expect_equivalent(coef(lm_fit),
                     coef(g),
-                    tol = 1e-4)
+                    tol = 1e-3)
+})
+
+test_that("wide and tall inputs work correctly", {
+
+  set.seed(926)
+
+  grid <- expand.grid(n = c(50, 100),
+                      p = c(50, 100),
+                      density = c(1, 0.5))
+
+  for (i in seq_len(nrow(grid))) {
+    n <- grid$n[i]
+    p <- grid$p[i]
+    d <- grid$density[i]
+
+    xy <- owl:::randomProblem(n, p, density = d)
+
+    expect_silent(owl(xy$x, xy$y, n_sigma = 5))
+  }
 })
